db3b5209192ba138f090fd7584e1507eac7158cc
Amanuel <105606795+amanyih@users.noreply.github.com>
2022-09-10 12:14:18 +0300

    Time: 116 ms (22.01%), Space: 13.9 MB (73.16%) - LeetHub

---
+++ 443-string-compression/443-string-compression.py
@@ -0,0 +1,28 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        ans = 0
+        left = 0
+        
+        while left < len(chars):
+            right = left + 1
+            while right < len(chars) and chars[left] == chars[right]:
+                right += 1
+            
+            length = right - left
+            if length == 1:
+                chars[ans]=chars[left]
+                ans += 1
+            else:
+                chars[ans] = chars[left]
+                ans += 1
+                for i in str(length):
+                    chars[ans] = i
+                    ans += 1
+            left = right
+        
+        return ans
+                    
+                
+                
+                
+        
