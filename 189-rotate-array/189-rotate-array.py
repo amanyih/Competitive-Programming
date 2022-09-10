b2cb25786e13edc37526062343dc05333cd21095
Amanuel <105606795+amanyih@users.noreply.github.com>
2022-09-10 15:33:47 +0300

    Time: 467 ms (31.96%), Space: 25.5 MB (7.02%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -1,8 +1,27 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        copyNums = [i for i in nums]
-        for index in range(len(nums)):
-            newIndex = (index+k) % len(nums)
-            nums[newIndex] = copyNums[index]
-            
+        left = 0
+        right = len(nums) - 1
+        k = k % len(nums)
+        while left < right:
+            nums[left],nums[right] = nums[right],nums[left]
+            left +=1
+            right -=1
         
+        left = 0
+        right = k-1
+        
+        while left < right:
+            nums[left],nums[right] = nums[right],nums[left]
+            left +=1
+            right -=1
+        
+        
+            
+        left = k
+        right = len(nums)-1
+        
+        while left < right:
+            nums[left],nums[right] = nums[right],nums[left]
+            left +=1
+            right -=1
