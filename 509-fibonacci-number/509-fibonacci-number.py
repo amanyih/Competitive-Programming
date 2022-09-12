3ad447ea2f2b092059faedab0487f491cffb436f
Amanuel <105606795+amanyih@users.noreply.github.com>
2022-09-12 23:18:56 +0300

    Time: 2360 ms (5.00%), Space: 14 MB (9.49%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        cache = {}
+        
+        if n == 0 or n == 1:
+            return n
+        else:
+            return self.fib(n-1) + self.fib(n-2)
+            
