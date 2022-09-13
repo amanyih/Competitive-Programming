a1ae20acf40cdda9459dda419f27caac492d6179
Amanuel <105606795+amanyih@users.noreply.github.com>
2022-09-13 15:11:21 +0300

    Time: 58 ms (26.15%), Space: 13.9 MB (19.88%) - LeetHub

--- 50-powx-n/50-powx-n.py
+++ 50-powx-n/50-powx-n.py
@@ -1,3 +1,13 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return pow(x,n)
+        def recursion(x,n):
+            if x == 0:
+                return 0
+            if n == 0:
+                return 1
+            
+            return recursion(x*x,n//2) if not n % 2 else x * recursion(x*x,n//2)
+        
+        ans = recursion(x,abs(n))
+        return ans if n >= 0 else 1/ans
+            
