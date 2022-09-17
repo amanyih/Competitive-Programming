163ca2637880f96cde7dc107a5f8ebd58c41483a
Amanuel <105606795+amanyih@users.noreply.github.com>
2022-09-17 15:36:46 +0300

    Time: 51 ms (79.41%), Space: 13.9 MB (29.71%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.py
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.py
@@ -1,26 +1,36 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def deleteDuplicates(self, head):
-        newNode = ListNode()
-        tail = newNode
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        dummyHead = ListNode()
+        left = dummyHead
+        secLeft = head
+        right = head
         
-        first = head
+        count = 0
         
-        while first:
-            count = 0
-            temp = first
-            while temp and temp.val == first.val:
-                count += 1
-                temp = temp.next
-            if count == 1:
-                tail.next = ListNode(val = first.val)
-                tail= tail.next
-            first = temp
-        
-        return newNode.next
+        while right:
+            if secLeft.val != right.val:
+                if count == 1:
+                    left.next = secLeft
+                    left = left.next
+                    left.next = None
+                secLeft = right
+                count = 0
+            count += 1
+            if not right.next and count == 1:
+                left.next = right
+            
+            right = right.next
+            
                 
+            
+        return dummyHead.next
+        
+        
         
