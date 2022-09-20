440696a6b3c1b67b0b56e777c1e26c729e10ccc1
Amanuel <105606795+amanyih@users.noreply.github.com>
2022-09-20 15:18:32 +0300

    Time: 761 ms (9.72%), Space: 16.6 MB (33.76%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
@@ -1,18 +1,24 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        dict_ = {0:1}
-        count = 0
-        totalSum = 0
-        
-        for num in nums:
-            totalSum += num
-            if totalSum - k in dict_:
-                count += dict_[totalSum-k]
-            if totalSum in dict_:
-                dict_[totalSum] += 1
-            else:
-                dict_[totalSum] = 1
-        
-        return count
             
+        prefixFreq = {0:1}
         
+        prefix = 0
+        ans = 0
+        for num in nums:
+            
+            prefix += num
+            
+            if (prefix - k) in prefixFreq:
+                ans += prefixFreq[prefix - k]
+            
+            if prefix in prefixFreq:
+                prefixFreq[prefix] += 1
+            else:
+                prefixFreq[prefix] = 1
+        
+        return ans
+    """
+    {0:1, 1:1, 3:1, 6:1}   ans = 1 + 1
+    """
+    
