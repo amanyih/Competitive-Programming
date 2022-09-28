8387d4b03b2cc16208d6810ac6ac2fa870ffe05e
Amanuel <105606795+amanyih@users.noreply.github.com>
2022-09-28 14:56:04 +0300

    Time: 36 ms, Memory: 13.8 MB - LeetHub

--- sort-colors/sort-colors.py
+++ sort-colors/sort-colors.py
@@ -4,13 +4,19 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         
-        for i in range(len(nums)):
-            minIndex = i
+        count = {0:0,1:0,2:0}
+        
+        for num in nums:
+            count[num] = count.get(num) + 1
             
-            for j in range(i,len(nums)):
-                if nums[j] < nums[minIndex]:
-                    minIndex = j
+        for i in range(len(nums)):
+            if count[0] != 0:
+                nums[i] = 0
+                count[0] = count.get(0) - 1
+            elif count[1] != 0:
+                nums[i] = 1
+                count[1] = count.get(1) - 1
+            else:
+                nums[i] = 2
+                count[2] = count.get(2) - 1
                 
-            nums[i],nums[minIndex] = nums[minIndex],nums[i]
-        
-        
