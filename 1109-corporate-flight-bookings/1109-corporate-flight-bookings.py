43e0310639fa16017af29e1e3523e15a97d5e46c
Amanuel <105606795+amanyih@users.noreply.github.com>
2022-09-30 15:56:30 +0300

    Time: 2147 ms (22.33%), Space: 28.4 MB (10.80%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.py
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.py
@@ -1,16 +1,18 @@
 class Solution:
     def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
-        ans = [0] * n
+        prefix = [0] *n
         
         for booking in bookings:
-            ans[booking[0] - 1] += booking[-1]
+            
+            prefix[booking[0]-1] += booking[-1]
             if booking[1] < n:
-                ans[booking[1]] -= booking[-1]
+                prefix[booking[1]] -= booking[-1]
         
-        curSum = 0
+        cur = 0
         
-        for i in range (n):
-            curSum += ans[i]
-            ans[i] = curSum
+        for i in range(n):
+            cur += prefix[i]
+            prefix[i] = cur
         
-        return ans
+        return prefix
+            
