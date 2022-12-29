b264f269d81555408b56793594ea1d68321d80b8
Amanuel <105606795+amanyih@users.noreply.github.com>
2022-12-29 19:39:36 +0300

    Time: 57 ms (88.59%), Space: 14 MB (39.76%) - LeetHub

--- 0811-subdomain-visit-count/0811-subdomain-visit-count.py
+++ 0811-subdomain-visit-count/0811-subdomain-visit-count.py
@@ -7,12 +7,12 @@ class Solution:
             
             lst = domain.split()
             freq = int(lst[0])
-            newDomain = deque(lst[1].split("."))
+            newDomain = lst[1].split(".")
             
             while newDomain:
                 dom = ".".join(newDomain)
                 count[dom] += freq
-                newDomain.popleft()
+                newDomain.pop(0)
         ans = []
         
         for key in count:
