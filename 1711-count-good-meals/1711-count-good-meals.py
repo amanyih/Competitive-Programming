e8b38be68ec59c3cef64f14d492bdb14cc28b29d
Amanuel <105606795+amanyih@users.noreply.github.com>
2023-01-05 23:09:32 +0300

    Time: 7160 ms (5.85%), Space: 24.3 MB (27.19%) - LeetHub

--- 1711-count-good-meals/1711-count-good-meals.py
+++ 1711-count-good-meals/1711-count-good-meals.py
@@ -1,27 +1,23 @@
 class Solution:
-    def countPairs(self, de: List[int]) -> int:
-        """
-        [2,4,8,16,32]
+    def countPairs(self, deliciousness: List[int]) -> int:
+        deliciousness.sort()
         
-        [1,3,5,7,9]
-         l r
-         [1,1,1,1]
-          0 1 2 3
-        """
-
-        possible_scores = [2**i for i in range(1,22)]
-        possible_scores.append(1)
-        de.sort()
-        no_delicious = 0
-        for target in possible_scores:
+        possibleScores = [2**i for i in range(22)]
+        
+        ans = 0
+        
+        for target in possibleScores:
             count = defaultdict(int)
-            for num in de:
-                subt = target - num
-                no_delicious += count[subt]
-                count[num] += 1
+            
+            for taste in deliciousness:
                 
-        return no_delicious % (10**9+7)
-                        
+                targetScore = target - taste
+                ans += count[targetScore]
+                count[taste] += 1
+        
+        return ans % (10**9 + 7)
                     
+                
             
             
+        
