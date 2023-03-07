79f8004f650283108704c1ef4c3750d06c912c7f
Amanuel Yihune <105606795+amanyih@users.noreply.github.com>
2023-03-07 21:14:06 +0300

    Time: 492 ms (27.84%), Space: 20.6 MB (28.60%) - LeetHub

---
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.py
@@ -0,0 +1,29 @@
+class Solution:
+    def smallestDivisor(self, nums: List[int], threshold: int) -> int:
+        
+        
+        
+        def sumOfQuotient(divisor):
+            res = 0
+            
+            for num in nums:
+
+                res += (ceil(num/divisor))
+            return res
+        
+    
+        left = 1
+        right = sum(nums)
+        
+        while left + 1 <= right:
+            mid = (right + left) // 2
+            
+            cur = sumOfQuotient(mid)
+            
+            if cur <= threshold:
+                right = mid
+            else:
+                left = mid +1
+        return right
+            
+            
