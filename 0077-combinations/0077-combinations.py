fd2f3891cb1cdb291fbd8a234593ab0ac3fd27d5
Amanuel Yihune <105606795+amanyih@users.noreply.github.com>
2023-03-09 11:16:13 +0300

    Time: 1612 ms (5.04%), Space: 16.2 MB (7.67%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -6,22 +6,14 @@ class Solution:
             
             if len(stack) == k:
                 newList = stack.copy()
-                # print(newList)
-                # print(len(set(newList)))
                 if len(set(newList)) == k:
-                    # print("asdd")
-                    # print(newList)
                     ans.append(newList)
-                stack.pop()
                 return
             current = [x for x in range(cur+1,n+1)]
-            # print(current)
-            # print(current)
             
             for num in current:
                 s = stack.copy()
                 s.append(num)
-                # print(num,stack)
                 helper(num,s)
         
         for i in range(n):
