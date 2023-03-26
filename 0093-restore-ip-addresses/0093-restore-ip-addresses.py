713f06158f6e1569806e95278d46a00461ab0a10
Amanuel Yihune <105606795+amanyih@users.noreply.github.com>
2023-03-26 20:59:54 +0300

    Time: 45 ms (30.23%), Space: 13.9 MB (71.59%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -4,22 +4,16 @@ class Solution:
         res = []
         
         def backtrack(path,cur):
-            # print(path,cur)
             if cur and len(path) == 3 and len(cur) == len(str(int(cur))) and 0<=int(cur)<= 255:
                 path.append(cur)
                 res.append(".".join(path))
                 path.pop()
                 return
             elif len(path) == 3 or cur == "":
-                # print("he")
                 return
-            
-            # print("forloop")
             for i in range(1,len(cur)+1):
-                # print(cur[:i])
-                
+
                 if 0<=int(cur[:i])<= 255 and  len(cur[:i]) == len(str(int(cur[:i]))):
-                    # print("onetrue")
                     path.append(cur[:i])
                     backtrack(path,cur[i:])
                     path.pop()
