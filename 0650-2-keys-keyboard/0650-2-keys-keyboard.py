fd05b87b7c497662061341763f32ffed112d0863
Amanuel Yihune <105606795+amanyih@users.noreply.github.com>
2023-09-18 10:43:13 +0300

    Time: 37 ms (92.36%), Space: 16.3 MB (80.14%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -1,14 +1,24 @@
 class Solution:
     def minSteps(self, n: int) -> int:
-        factorization: list[int] = []
-        d = 2
+        
+        cache = {1 :0,2:2}
+        
+        def findFactor(n):
+            
+            for i in range(n-1,int(sqrt(n))-1,-1):
+                if n % i == 0:
+                    return i
+            return 1
+        
+        def find(n):
+            if n in cache:
+                return cache[n]
+            
+            factor = findFactor(n)
+             
+            cur = find(factor) + n//factor
+            cache[n] = cur
+            return cur
+        return find(n)
         
         
-        while d * d <= n:
-            while n % d == 0:
-                factorization.append(d)
-                n //= d
-            d += 1
-        if n > 1:
-            factorization.append(n)
-        return sum(factorization)
