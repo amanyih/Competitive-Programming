a5df887dfc335a5f5eeb594c9272b244efe83f2f
Amanuel Yihune <105606795+amanyih@users.noreply.github.com>
2023-10-03 15:10:59 +0300

    Time: 629 ms (18.98%), Space: 23.9 MB (59.90%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -1,60 +1,26 @@
 class Solution:
-    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+    def eventualSafeNodes(self, lst: List[List[int]]) -> List[int]:
         
+        graph = defaultdict(list)
+        indegree = defaultdict(int)
         
-        lst = defaultdict(list)
-        terminals = set()
-        isTerminal = {}
-        for i,g in enumerate(graph):
-            lst[i] = g
-            if not len(g):
-                terminals.add(i)
-        
-        
-        visited = set()
-        
-        def dfs(node):
-            if node in isTerminal:
-                return isTerminal[node]
-            elif node in terminals:
-                return True
-            visited.add(node)
-            cur = []
-            
-            for n in lst[node]:
+        for i,nodes in enumerate(lst):
+            for node in nodes:
+                graph[node].append(i)
+                indegree[i] += 1
+        q = deque()
+        for node in range(len(lst)):
+            if indegree[node] == 0:
+                q.append(node)
+        res = []
+        while q:
+            cur = q.popleft()
+            res.append(cur)
+            for node in graph[cur]:
+                indegree[node] -=1
+                if indegree[node] == 0:
+                    q.append(node)
+        return sorted(res)
                 
-                if n in isTerminal:
-                    cur.append(isTerminal[n])
-                elif n in terminals:
-                    cur.append(True)
             
-                elif n in visited:
-                    cur.append(False)
-                else:
-                    cur.append(dfs(n))
-            
-            val = all(cur)
-            isTerminal[node]  = val
-            return val
-        ans = []
-        for node in lst:
-            if dfs(node):
-                ans.append(node)
-        
-        ans.sort()
-        
-        return ans
-                
-        
-                    
-            
-            
-            
-
-            
-            
-        
-        
-            
-        
         
