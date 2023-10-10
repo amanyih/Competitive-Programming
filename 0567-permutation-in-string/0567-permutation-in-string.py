3587bc8780d96a39c993cf0786851a3922fc6243
Amanuel Yihune <105606795+amanyih@users.noreply.github.com>
2023-10-10 16:07:48 +0300

    Time: 61 ms (85.15%), Space: 16.5 MB (40.21%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -2,22 +2,22 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         
         winSize = len(s1)
-        count = {}
-        for char in s1:
-            count[char] = count.get(char,0) + 1
+        countS1 = {}
         
-        curCount = {}
+        for i in range(len(s1)):
+            countS1[s1[i]] = countS1.get(s1[i],0) + 1
         
         left = 0
+        cur = {}
         for right in range(len(s2)):
-            curCount[s2[right]] = curCount.get(s2[right],0) + 1
+            cur[s2[right]] = cur.get(s2[right],0) + 1
             
+            if cur == countS1:
+                return True
             if right - left + 1 == winSize:
-                if curCount == count:
-                    return True
+                cur[s2[left]] -= 1
+                if cur[s2[left]] == 0:
+                    del cur[s2[left]]
                 
-                curCount[s2[left]] -= 1
-                if curCount[s2[left]] == 0:
-                    del curCount[s2[left]]
                 left += 1
         return False
