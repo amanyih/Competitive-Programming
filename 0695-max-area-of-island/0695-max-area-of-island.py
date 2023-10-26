febdd8343ecfbfcd75b64d69f59edb7cfd2fc8f6
Amanuel Yihune <105606795+amanyih@users.noreply.github.com>
2023-10-27 02:11:32 +0300

    Time: 148 ms (15.48%), Space: 19.4 MB (51.37%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -2,30 +2,35 @@ class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         
         directions = [(-1,0),(1,0),(0,1),(0,-1)]
+        
+        def valid(row,col):
+            return 0 <= row < len(grid) and 0 <= col < len(grid[0])
+        
         visited = set()
         
+        def dfs(row,col,cur_visited):
+            cur_visited.add((row,col))
+            
+            for r,c in directions:
+                new_r,new_c = row + r, col + c
+                if valid(new_r,new_c) and grid[new_r][new_c] and (new_r,new_c) not in cur_visited:
+                    cur_visited.add((new_r,new_c))
+                    dfs(new_r,new_c,cur_visited)
+        
         ans = 0
-        
-        def explore(row,col,count):
-            
-            visited.add((row,col))
-            count.append(1)
-            
-            for direction in directions:
-                r,c = direction
-                
-                if 0 <= row + r < len(grid) and 0<= col + c < len(grid[0]) and grid[row+r][col+c] == 1 and (row+r,col+c) not in visited:
-                    explore(row+r,col+c,count)
-            return sum(count)
-        
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
-                if grid[r][c] == 1 and (r,c) not in visited:
-                    cur = explore(r,c,[])
-                    ans = max(ans,cur)
-        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] and (i,j ) not in visited:
+                    cur_visited = set()
+                    dfs(i,j,cur_visited)
+                    # print(i,j,len(cur_visited))
+                    ans = max(ans,len(cur_visited))
+                    visited = visited.union(cur_visited)
         return ans
+                    
+                    
             
-            
-            
-            
+        
+        
+        
+        
